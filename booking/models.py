240834b16acff6ab6dca862c9767da9d95c49a9d
LADCode2021 <luke.dawson@live.co.uk>
2022-09-17 17:28:49 +0000

    Add Bookings class  to models.py

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Bookings(models.Model):
+    first_name = models.CharField(max_length=25, null=False, blank=False)
+    last_name = models.CharField(max_length=25, null=False, blank=False)
+    phone_number = models.BigIntegerField(max_length=11)
+    date_of_booking = models.DateField()
+    time_of_booking = models.TimeField()
+    number_of_people = models.IntegerField()
+    special_requests = models.CharField(max_length=200)
+
+
