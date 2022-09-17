8129042b1fc5afd52cee5f6d7f9dabbc960be186
LADCode2021 <luke.dawson@live.co.uk>
2022-09-17 18:35:48 +0000

    Migrate Booking class to sqlite database to create Booking table. Update __str__ in models.py

--- booking/models.py
+++ booking/models.py
@@ -2,13 +2,24 @@ from django.db import models
 
 # Create your models here.
 
-class Bookings(models.Model):
+class Booking(models.Model):
     first_name = models.CharField(max_length=25, null=False, blank=False)
     last_name = models.CharField(max_length=25, null=False, blank=False)
-    phone_number = models.BigIntegerField(max_length=11)
+    phone_number = models.IntegerField()
     date_of_booking = models.DateField()
     time_of_booking = models.TimeField()
     number_of_people = models.IntegerField()
     special_requests = models.CharField(max_length=200)
+
+    def __str__(self): #code adapted from Models Part 2 FST walkthrough
+        return self.first_name + " " + self.last_name + " | " \
+             + str(self.date_of_booking) + " at " + str(self.time_of_booking)
+
+
+
+
+
+
+
 
 
