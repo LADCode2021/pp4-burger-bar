c64c5b313ad3c391dc09de54822a40e0b370e3c0
LADCode2021 <luke.dawson@live.co.uk>
2022-09-18 22:01:55 +0000

    Install Heroku. Install dj_database_url. Update settings.py. Migrate database

--- burger_bar/settings.py
+++ burger_bar/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'burger_bar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse('postgres://pictttcuvnqluc:afab797897d1545a13e3d053d84379cbac11b048b5a3ae50ef32ec8b56df0313@ec2-34-248-169-69.eu-west-1.compute.amazonaws.com:5432/ddlq3rr3eg4dla')
 }
 
 
