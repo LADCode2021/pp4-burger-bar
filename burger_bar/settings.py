43ada8d7e274440ac511e2867e85bef4d6c672b5
LADCode2021 <luke.dawson@live.co.uk>
2022-09-19 07:55:28 +0000

    Update settings.py to hid SECRET_KEY

--- burger_bar/settings.py
+++ burger_bar/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+if os.path.exists("env.py"):
+    import env  # noqa
 
 development = os.environ.get('DEVELOPMENT', False)
 
@@ -23,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-9v7!5wixw!(pniefsjvcj@%z2*aa=lqnbm35+ygbnz^-#qh3#z')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
